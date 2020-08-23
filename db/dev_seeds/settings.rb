2d42ac1911b06d249a1fb0d290ff9027cbd8a2cc
Carlos Serrada <cserradag96@gmail.com>
2020-08-23 17:52:03 -0400

    Update seeds for settings

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -2,25 +2,24 @@ section "Creating Settings" do
   Setting.reset_defaults
 
   {
-    "facebook_handle": "CONSUL",
+    "facebook_handle": "KAVAK Petitions",
     "feature.featured_proposals": "true",
     "feature.map": "true",
-    "instagram_handle": "CONSUL",
+    "instagram_handle": "KAVAK Petitions",
     "mailer_from_address": "noreply@consul.dev",
-    "mailer_from_name": "CONSUL",
-    "meta_description": "Citizen participation tool for an open, "\
-                        "transparent and democratic government",
-    "meta_keywords": "citizen participation, open government",
-    "meta_title": "CONSUL",
-    "org_name": "CONSUL",
+    "mailer_from_name": "KAVAK Petitions",
+    "meta_description": "Free software for kavakers participation.",
+    "meta_keywords": "kavak",
+    "meta_title": "KAVAK Petitions",
+    "org_name": "KAVAK Petitions",
     "proposal_code_prefix": "MAD",
     "proposal_notification_minimum_interval_in_days": 0,
-    "telegram_handle": "CONSUL",
-    "twitter_handle": "@consul_dev",
-    "twitter_hashtag": "#consul_dev",
+    "telegram_handle": "KAVAK Petitions",
+    "twitter_handle": "",
+    "twitter_hashtag": "",
     "url": "http://localhost:3000",
     "votes_for_proposal_success": "100",
-    "youtube_handle": "CONSUL"
+    "youtube_handle": ""
   }.each do |name, value|
     Setting[name] = value
   end
